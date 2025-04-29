81da580b3bb1c8099bfc5c94c8e5ed3027ff5a1f
wonjin <168061167+dustkim@users.noreply.github.com>
2025-04-29 15:38:15 +0900

    Delete step6 directory

--- step6/별 찍기 - 7.py
+++
@@ -1,9 +0,0 @@
-# baekjoon no.2444
-
-n = int(input())
-
-for i in range(n):
-    print(" "*(n-i-1)+"*"*(i*2+1))
-
-for i in range(n-1):
-    print(" "*(i+1)+"*"*(2*(n-i-1)-1))
--- step6/새싹.py
+++
@@ -1,8 +0,0 @@
-# baekjoon no.25083
-
-print("         ,r\'\"7")
-print("r`-_   ,\'  ,/")
-print(" \\. \". L_r\'")
-print("   `~\\/")
-print("      |")
-print("      |")
